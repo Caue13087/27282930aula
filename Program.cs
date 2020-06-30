8bb46483e201fa2372a5de8e2a5eca1a49692517
Caue13087 <caue13087@gmail.com>
2020-06-30 15:42:06 -0300

    aula27282930desafio

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Aula27_28_29_30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto();
+            p.Codigo = 8;
+            p.Nome = "Gragas";
+            p.Preco = 15f;
+
+            p.Inserir(p);
+
+            List<Produto> lista = p.Ler();
+
+            foreach(Produto item in lista){
+                Console.ForegroundColor = ConsoleColor.Blue;
+                System.Console.WriteLine($"R$:{item.Preco} - {item.Nome}");
+            }
+
+            //Buscar por Nome através da expressão lambda
+            Produto caue = lista.Find(x => x.Nome == "Gnar");
+
+            System.Console.WriteLine(caue.Nome);
+
+            
+
+            
+        }
+        
+        
+    }
+}
